22f42052c77f7f13b0ffe786b914ad522b3785a2
twolfe21 <vonwolfee@gmail.com>
2020-02-22 11:11:54 -0700

    update footer and social links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="footer">
+    <ul class="social_links">
+      <li>
+        <a target="_blank" href="https://github.com/twolfe21/">
+          <img src="@/assets/github_icon.png" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="https://twitter.com/tonyvonwolfe">
+          <img src="@/assets/twitter_icon.png" />
+        </a>
+      </li>
+    </ul>
     <span>
-      <a href="https://github.com/twolfe21/personal_blog">
-        <img src="@/assets/github_icon.png" />
-      </a>
-    </span>
-    <span>
-      <p>&copy; {{ name }} {{ new Date().getFullYear() }}</p>
+      <p>&copy; Tony Von Wolfe {{ new Date().getFullYear() }}</p>
     </span>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    name: {
-      type: String,
-      default: ''
-    }
-  }
-}
-</script>
-
 <style scoped>
+.social_links {
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  padding: 0;
+}
+
+.social_links li {
+  padding-right: 10px;
+}
+
+li + li {
+  border-left: 1px solid #4c4c4c;
+  padding-left: 10px;
+}
+
 #footer {
   display: flex;
   flex-direction: row;
